91d6549155ef9d2651af2003db25230a0069bbf6
Eric-Formiga <eric.13formiga@gmail.com>
2025-05-10 17:47:54 -0300

    feat: updating products

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,30 +1,50 @@
-import { NextFunction, Request, Response  } from "express";
-import {z} from "zod"
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod"
 import { knex } from "@/database/knex";
 
-class ProductController{
-  async index(request:Request, response: Response, next: NextFunction){
+class ProductController {
+  async index(request: Request, response: Response, next: NextFunction) {
     try {
-      const {name} = request.query
+      const { name } = request.query
 
-      const products = await knex<ProductRepository>("products").select().whereLike('name', `%${name ?? ""}%`).orderBy("name")
-
+      const products = await knex<ProductRepository>("products").select().whereLike('name', `%${name ?? ''}%`).orderBy("name")
+      
       return response.json(products)
+
     } catch (error) {
       next(error)
     }
   }
 
-  async create(request:Request, response: Response, next: NextFunction){
+  async create(request: Request, response: Response, next: NextFunction) {
     try {
       const bodySchema = z.object({
         name: z.string().trim().min(6),
-        price: z.number().gt(0, {message: "Value must be greather than 0"})
+        price: z.number().gt(0, { message: "Value must be greather than 0" })
       })
 
-      const {name, price} = bodySchema.parse(request.body)
+      const { name, price } = bodySchema.parse(request.body)
 
-      await knex<ProductRepository>("products").insert({name,price})
+      await knex<ProductRepository>("products").insert({ name, price })
+
+      return response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), { message: "id Must be a number" }).parse(request.params.id)
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "Value must be greather than 0" })
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").update({ name, price, updated_at: knex.fn.now() }).where({ id })
 
       return response.status(201).json()
     } catch (error) {
