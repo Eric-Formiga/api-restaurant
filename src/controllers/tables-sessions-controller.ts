d1b247371df7d5b5d8ba3e04a058a46da55f0bad
Eric-Formiga <eric.13formiga@gmail.com>
2025-05-17 16:05:46 -0300

    feat: add create table session

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -6,9 +6,25 @@ import { knex } from "@/database/knex";
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-      // const tables = await knex<TableRepository>("tables").select().orderBy("table_number")
 
-      // return response.json(tables)
+      const bodySchema = z.object({
+        table_id: z.number()
+      })
+
+      const { table_id } = bodySchema.parse(request.body)
+
+      const session = await knex<TableSessionsRepository>("tables_sessions").where({table_id}).orderBy("opened_at", "desc").first()
+
+      if(session && !session.closed_at){
+        throw new AppError("This table is already open")
+      }
+
+      await knex<TableSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      })
+
+      
       return response.status(201).json()
 
     } catch (error) {
